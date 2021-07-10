afbd92426859789ec23146fd41c9ca8a936f3a9f
Purna Gurung <purnagurung903@gmail.com>
2021-07-09 22:28:56 -0400

    added sidebar and Chat components

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAHv7ewujliAPotthe0kGh5iHx0RwuByI",
+  authDomain: "whatsapp-mern-ff65e.firebaseapp.com",
+  projectId: "whatsapp-mern-ff65e",
+  storageBucket: "whatsapp-mern-ff65e.appspot.com",
+  messagingSenderId: "586205331199",
+  appId: "1:586205331199:web:90789a1076449d5fc216ca",
+  measurementId: "G-XCNVCCF4TL"
+};
