e36f9030350645582c719dac1654043612561234
Purna Gurung <purnagurung903@gmail.com>
2021-09-20 10:22:32 -0400

    styled and added search bar

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -6,9 +6,10 @@ import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="app">
-      
+      <div className="app_body">
       <Sidebar />
       <Chat />
+      </div>
     </div>
   );
 }
--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined } from "@material-ui/icons";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://api.hub.jhu.edu/factory/sites/default/files/styles/landscape/public/monkey092018.jpg?itok=dwu7HH3a"/>
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+         </IconButton>
+         <IconButton>
+            <ChatIcon />
+         </IconButton>
+         <IconButton>
+            <MoreVertIcon />
+         </IconButton>
+        </div>
+
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
+ 
