1b6dfa2813626b7905d05daf70073c4c7f08f42b
Purna Gurung <purnagurung903@gmail.com>
2021-10-07 15:27:15 -0400

    done front end ready for backend

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,6 +1,9 @@
 import { Avatar, IconButton} from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import React from 'react'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons//Mic';
+
 import "./Chat.css"
 
 function Chat() {
@@ -44,87 +47,19 @@ function Chat() {
              .toUTCString()}
            </span>
            </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
            
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
            
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message chat__reciever">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
-           <p className="chat__message">
-           <span className="chat__name">Purna</span>
-           
-           This is a message
-           <span className="chat__timestamp">
-             {new Date()
-             .toUTCString()}
-           </span>
-           </p>
+      </div>
+      <div className="chat__footer">
+          <InsertEmoticonIcon />
+          <form>
+            <input placeholder="Type a message"
+            type="text" />
+            <button type="submit">
+              send a message
+            </button>
+          </form>
+          <MicIcon />
       </div>
     
     </div>
